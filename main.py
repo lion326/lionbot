4970d83c924816d2bbb048979c719edb04854d53
lion <kabushiki0326@yahoo.co.jp>
2022-01-22 16:55:15 +0900

    test

--- main.py
+++ main.py
@@ -40,9 +40,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    if event.message.text == "こんにちは":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="どうもこんにちは"))
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
