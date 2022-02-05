202a789e96e31cb78a84c18c5ad40bc2c6acd3bb
lion326 <kabushiki0326@yahoo.co.jp>
2022-02-05 16:59:22 +0900

    image4

--- main.py
+++ main.py
@@ -49,18 +49,14 @@ def handle_message(event):
         returnmessage = "お疲れ様でした"
     elif event.message.text == "ネオテック":
         # 社長の画像送信
-        line_bot_api.reply_message(
-            event.reply_token,
-            ImageSendMessage(
-                original_content_url="http://neotec-n.com/wp/wp-content/uploads/2016/07/syatyou02.jpg",
-                preview_image_url="http://neotec-n.com/wp/wp-content/uploads/2016/07/syatyou02.jpg"))
+        neotecimage(event)
     # 返信
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=returnmessage))
 
 
-# def neotecimage(event):
+def neotecimage(event):
     # messages = ImageSendMessage(
     #     # JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
     #     original_content_url="http://neotec-n.com/wp/wp-content/uploads/2016/07/syatyou02.jpg",
@@ -70,12 +66,11 @@ def handle_message(event):
     # line_bot_api.reply_message(
     #     event.reply_token,
     #     messages)
-
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     ImageSendMessage(
-    #         original_content_url="http://neotec-n.com/wp/wp-content/uploads/2016/07/syatyou02.jpg",
-    #         preview_image_url="http://neotec-n.com/wp/wp-content/uploads/2016/07/syatyou02.jpg"))
+    line_bot_api.reply_message(
+        event.reply_token,
+        ImageSendMessage(
+            original_content_url="https://1.bp.blogspot.com/-7uiCs6dI4a0/YEGQA-8JOrI/AAAAAAABddA/qPFt2E8vDfQwPQsAYLvk4lowkwP-GN7VQCNcBGAsYHQ/s896/buranko_girl_smile.png",
+            preview_image_url="https://1.bp.blogspot.com/-7uiCs6dI4a0/YEGQA-8JOrI/AAAAAAABddA/qPFt2E8vDfQwPQsAYLvk4lowkwP-GN7VQCNcBGAsYHQ/s896/buranko_girl_smile.png"))
 
 
 if __name__ == "__main__":
