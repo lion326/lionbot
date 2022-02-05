4ea4ee02c8d63f4a467d42768af702a55e7f91e1
lion326 <kabushiki0326@yahoo.co.jp>
2022-02-05 17:01:46 +0900

    image5

--- main.py
+++ main.py
@@ -57,20 +57,20 @@ def handle_message(event):
 
 
 def neotecimage(event):
-    # messages = ImageSendMessage(
-    #     # JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
-    #     original_content_url="http://neotec-n.com/wp/wp-content/uploads/2016/07/syatyou02.jpg",
-    #     # JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
-    #     preview_image_url="http://neotec-n.com/wp/wp-content/uploads/2016/07/syatyou02.jpg"
-    # )
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     messages)
+    messages = ImageSendMessage(
+        # JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+        original_content_url="https://1.bp.blogspot.com/-7uiCs6dI4a0/YEGQA-8JOrI/AAAAAAABddA/qPFt2E8vDfQwPQsAYLvk4lowkwP-GN7VQCNcBGAsYHQ/s896/buranko_girl_smile.png",
+        # JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+        preview_image_url="https://1.bp.blogspot.com/-7uiCs6dI4a0/YEGQA-8JOrI/AAAAAAABddA/qPFt2E8vDfQwPQsAYLvk4lowkwP-GN7VQCNcBGAsYHQ/s896/buranko_girl_smile.png"
+    )
     line_bot_api.reply_message(
         event.reply_token,
-        ImageSendMessage(
-            original_content_url="https://1.bp.blogspot.com/-7uiCs6dI4a0/YEGQA-8JOrI/AAAAAAABddA/qPFt2E8vDfQwPQsAYLvk4lowkwP-GN7VQCNcBGAsYHQ/s896/buranko_girl_smile.png",
-            preview_image_url="https://1.bp.blogspot.com/-7uiCs6dI4a0/YEGQA-8JOrI/AAAAAAABddA/qPFt2E8vDfQwPQsAYLvk4lowkwP-GN7VQCNcBGAsYHQ/s896/buranko_girl_smile.png"))
+        messages)
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     ImageSendMessage(
+    #         original_content_url="https://1.bp.blogspot.com/-7uiCs6dI4a0/YEGQA-8JOrI/AAAAAAABddA/qPFt2E8vDfQwPQsAYLvk4lowkwP-GN7VQCNcBGAsYHQ/s896/buranko_girl_smile.png",
+    #         preview_image_url="https://1.bp.blogspot.com/-7uiCs6dI4a0/YEGQA-8JOrI/AAAAAAABddA/qPFt2E8vDfQwPQsAYLvk4lowkwP-GN7VQCNcBGAsYHQ/s896/buranko_girl_smile.png"))
 
 
 if __name__ == "__main__":
